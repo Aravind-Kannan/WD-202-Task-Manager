153e39d9437987e10a2aed750c013b92ab1a47c3
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2022-02-19 12:02:56 +0300

    changed LOGIN_REDIRECT_URL

--- config/settings/base.py
+++ config/settings/base.py
@@ -108,7 +108,7 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "auth.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "/user/login" #"users:redirect"
+LOGIN_REDIRECT_URL = "/tasks" #"users:redirect"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "/user/login" #"account_login"
 LOGOUT_REDIRECT_URL = "/user/login"
