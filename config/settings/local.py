cbabb6ff55a35b84a3eb2491d1ca03cb9b055e44
Aravind Kannan Rathinasabapathi <aravindkannan2001@gmail.com>
2022-02-19 11:23:00 +0300

    update ALLOWED_HOSTS

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="wvm16jxC0PrZUN5Xz0pHgxXqEkmTlkAdOrw5TwhZbmwIoZYh1qWPQQNeXOsbZqsD",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "aravind-kannan-task-manager.herokuapp.com"]
 
 # CACHES
 # ------------------------------------------------------------------------------
